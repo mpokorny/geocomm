3e60733a3689f4316a2feeb09ea33d5aedde7a7b
Martin Pokorny <martin@truffulatree.org>
2015-06-27 19:08:31 -0600

    Initial version of csv conversion program

---
+++ csv2LatLon/src/main/scala/org/truffulatree/geocomm/CSV2LatLon.scala
@@ -0,0 +1,9 @@
+package org.truffulatree.geocomm
+
+import scalaz.effect.{ IO, SafeApp }
+
+// object CSV2LatLon extends SafeApp {
+//   override def runl(args: List[String]): IO[Unit] = {
+
+//   }
+// }
