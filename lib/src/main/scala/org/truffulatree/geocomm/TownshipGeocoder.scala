802c8f59b8e8b6be1b2453e003a49bbd896419d0
Martin Pokorny <martin@truffulatree.org>
2015-07-06 15:58:46 -0600

    Regulate requests to web service
    The TownshipGeoCoder web service seems to be unreliable when quickly
    firing off requests...this patch limits the number of outstanding
    requests to the web service to one at a time.

--- lib/src/main/scala/org/truffulatree/geocomm/TownshipGeocoder.scala
+++ lib/src/main/scala/org/truffulatree/geocomm/TownshipGeocoder.scala
@@ -1,7 +1,7 @@
 package org.truffulatree.geocomm
 
 import java.util.Date
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.Future
 import scala.xml
 import scalaz._
 import Scalaz._
@@ -13,6 +13,8 @@ object TownshipGeocoder {
 
   val townshipGeocoder = url(
     "http://www.geocommunicator.gov/TownshipGeoCoder/TownshipGeoCoder.asmx/GetLatLon")
+
+  var lastRequest = Future.successful[xml.Elem](<empty/>)
 
   def trsProps(trs: TRS): String = {
     List(
@@ -30,11 +32,15 @@ object TownshipGeocoder {
   }
 
   def request(trs: TRS): Future[xml.Elem] = {
+    // Slamming the geocoder web service as quickly as possible produces
+    // unreliable results, so we limit ourselves to only one outstanding request
+    // at a time. Whether that is that best policy is undetermined.
     val query = townshipGeocoder <<? Map("TRS" -> trsProps(trs))
-    Http(query OK as.xml.Elem)
+    lastRequest = lastRequest >> Http(query OK as.xml.Elem)
+    lastRequest
   }
 
-  val getData: (xml.Elem) => \/[Throwable, xml.Elem] = elem =>
+  def getData: (xml.Elem) => \/[Throwable, xml.Elem] = elem =>
     if (elem.label == "TownshipGeocoderResult")
       \/.fromTryCatchNonFatal {
         (elem \ "CompletionStatus").text match {
@@ -46,10 +52,10 @@ object TownshipGeocoder {
       } flatMap (identity)
     else (new Exception("Unexpected response")).left
 
-  val parseRss: (xml.Elem) => \/[Throwable, RSSChannel] =
+  def parseRss: (xml.Elem) => \/[Throwable, RSSChannel] =
     RSS.parse _
 
-  val selectTownshipGeoCoder: (RSSChannel) => \/[Throwable, RSSChannel] = ch =>
+  def selectTownshipGeoCoder: (RSSChannel) => \/[Throwable, RSSChannel] = ch =>
     if (ch.title == "Township GeoCoder") ch.right
     else (new Exception("'Township GeoCoder' element not found")).left
 
@@ -64,10 +70,10 @@ object TownshipGeocoder {
           new Exception(s"'$itemName' item not found")))
     }
 
-  val selectLatLon: (RSSChannel) => \/[Throwable, RSSItem] =
+  def selectLatLon: (RSSChannel) => \/[Throwable, RSSItem] =
     selectGeoCoderItem("Lat Lon")
 
-  val parseLatLon: (RSSItem) => \/[Throwable, (Double, Double)] = ch =>
+  def parseLatLon: (RSSItem) => \/[Throwable, (Double, Double)] = ch =>
     (ch.content filter { node =>
       node.label == "point" && node.namespace == "http://www.georss.org/georss"
     } map { node =>
@@ -78,15 +84,15 @@ object TownshipGeocoder {
       }
     }).headOption.getOrElse((new Exception("Failed to find 'point' item")).left)
 
-  val getTownshipGeoCoder = 
+  def getTownshipGeoCoder =
     getData(_: xml.Elem) >>= parseRss >>= selectTownshipGeoCoder
 
-  val extractLatLon = selectLatLon(_: RSSChannel) >>= parseLatLon
+  def extractLatLon = selectLatLon(_: RSSChannel) >>= parseLatLon
 
-  val selectTownshipRangeSection: (RSSChannel) => \/[Throwable, RSSItem] =
+  def selectTownshipRangeSection: (RSSChannel) => \/[Throwable, RSSItem] =
     selectGeoCoderItem("Township Range Section")
 
-  val parseTownshipRangeSection:
+  def parseTownshipRangeSection:
       (RSSItem) => \/[Throwable, List[(Double, Double)]] = ch => {
     (ch.content filter { node =>
       node.label == "polygon" &&
@@ -101,173 +107,17 @@ object TownshipGeocoder {
       getOrElse((new Exception("Failed to find 'polygon' item")).left)
   }
 
-  val extractTownshipRangeSection = 
+  def extractTownshipRangeSection =
     selectTownshipRangeSection(_: RSSChannel) >>= parseTownshipRangeSection
 
   def getPart[A](parser: (RSSChannel) => \/[Throwable, A]):
       xml.Elem => \/[Throwable, A] =
     getTownshipGeoCoder(_: xml.Elem) >>= parser
 
-  val getLatLon: (xml.Elem) => \/[Throwable, (Double, Double)] =
+  def getLatLon: (xml.Elem) => \/[Throwable, (Double, Double)] =
     getPart(extractLatLon)
 
-  val getTownshipRangeSection: 
+  def getTownshipRangeSection: 
       (xml.Elem) => \/[Throwable, List[(Double, Double)]] =
     getPart(extractTownshipRangeSection)
-
-  // type Requested =
-  //   RecPlus[ValidationNel[Throwable, (Deadline, Future[xml.Elem])]]
-  // type IoRequested = IoExceptionOr[Requested]
-
-  // def sendRequest(requestTimeout: FiniteDuration):
-  //     EnumerateeT[IoParsed, IoRequested, IO] =
-  //   Iteratee.map { ioparsed =>
-  //     ioparsed map {
-  //       case (recNum, sep, rec, vtrs) =>
-  //         (recNum, sep, rec, vtrs map (trs =>
-  //           (Deadline.now + requestTimeout, TownshipGeocoder.request(trs))))
-  //     }
-  //   }
-
-  // type Response = RecPlus[\/[NonEmptyList[Throwable],xml.Elem]]
-  // type IoResponse = IoExceptionOr[Response]
-
-  // def completeRequest(req: Requested): Response = req match {
-  //   case (recNum, sep, rec, vreq) =>
-  //     (recNum,
-  //       sep,
-  //       rec,
-  //       vreq.fold(
-  //         ths => ths.left,
-  //         fr => fr match {
-  //           case (deadline, future) =>
-  //             if (future.isCompleted) 
-  //               Await.result(future, -1.seconds).right
-  //             else
-  //               NonEmptyList(
-  //                 new TimeoutException("ERROR: Request timed out")).left
-  //         }))
-  // }
- 
-  // def partitionComplete(reqs: Vector[Requested]):
-  //     (Vector[Response], Vector[Requested]) = {
-  //   val (c, uc) = reqs partition {
-  //     case (_, _, _, vreq) => 
-  //       vreq.isFailure || vreq.exists {
-  //         case (deadline, future) =>
-  //           future.isCompleted || deadline.isOverdue
-  //       }
-  //   }
-  //   (c map (completeRequest _), uc)
-  // }
-
-  // def getResponses: EnumerateeT[IoRequested, IoResponse, IO] =
-  //   new EnumerateeT[IoRequested, IoResponse, IO] {
-  //     def apply[A] = {
-  //       def nextResponseOrCont(
-  //         rsps: Vector[Response],
-  //         reqs: Vector[Requested],
-  //         k: (Input[IoResponse] => IterateeT[IoResponse, IO, A])):
-  //           IterateeT[IoRequested, IO, StepT[IoResponse, IO, A]] = {
-  //         val (rsps1, reqs1) = {
-  //           val (newRsps, allReqs) = partitionComplete(reqs)
-  //           (rsps ++ newRsps, allReqs)
-  //         }
-  //         rsps1.headOption map { rsp =>
-  //           k(elInput(IoExceptionOr(rsp))) >>== doneOr(loop(rsps1.tail, reqs1))
-  //         } getOrElse {
-  //           cont(step(rsps1, reqs1)(k))
-  //         }
-  //       }
-
-  //       def loop(rsps: Vector[Response], reqs: Vector[Requested]) =
-  //         step(rsps, reqs) andThen cont[IoRequested, IO, StepT[IoResponse, IO, A]]
-
-  //       def step(rsps: Vector[Response], reqs: Vector[Requested]):
-  //           ((Input[IoResponse] => IterateeT[IoResponse, IO, A]) =>
-  //             Input[IoRequested] =>
-  //             IterateeT[IoRequested, IO, StepT[IoResponse, IO, A]]) = {
-  //         k => in => {
-  //           in(
-  //             el = ioreq => {
-  //               ioreq.fold(
-  //                 exc => drainNextResponseOrContOrDone(rsps, reqs, exc.some, k, in),
-  //                 req => nextResponseOrCont(rsps, reqs :+ req, k))
-  //             },
-  //             empty = nextResponseOrCont(rsps, reqs, k),
-  //             eof = drainNextResponseOrContOrDone(rsps, reqs, none, k, in))
-  //         }
-  //       }
-
-  //       def drainNextResponseOrContOrDone(
-  //         rsps: Vector[Response],
-  //         reqs: Vector[Requested],
-  //         oex: Option[IoExceptionOr.IoException],
-  //         k: Input[IoResponse] => IterateeT[IoResponse, IO, A],
-  //         in: Input[IoRequested]): 
-  //           IterateeT[IoRequested, IO, StepT[IoResponse, IO, A]] = {
-  //         val (rsps1, reqs1) = {
-  //           val (newRsps, allReqs) = partitionComplete(reqs)
-  //           (rsps ++ newRsps, allReqs)
-  //         }
-  //         rsps1.headOption map { rsp =>
-  //           k(elInput(IoExceptionOr(rsp))) >>==
-  //           doneOr(drainLoop(rsps1.tail, reqs1, oex))
-  //         } getOrElse {
-  //           if (!reqs.isEmpty) {
-  //             cont(drainStep(rsps1, reqs1, oex)(k))
-  //           }
-  //           else {
-  //             oex map { ex =>
-  //               done[IoRequested, IO, StepT[IoResponse, IO, A]](
-  //                 scont(k), Input(IoExceptionOr.ioException(ex)))
-  //             } getOrElse {
-  //               done[IoRequested, IO, StepT[IoResponse, IO, A]](
-  //                 scont(k), in)
-  //             }
-  //           }
-  //         }
-  //       }
-
-  //       def drainLoop(
-  //         rsps: Vector[Response], 
-  //         reqs: Vector[Requested], 
-  //         ex: Option[IoExceptionOr.IoException]) =
-  //         drainStep(rsps, reqs, ex).andThen(
-  //           cont[IoRequested, IO, StepT[IoResponse, IO, A]])
-
-  //       def drainStep(
-  //         rsps: Vector[Response],
-  //         reqs: Vector[Requested],
-  //         ex: Option[IoExceptionOr.IoException]):
-  //           ((Input[IoResponse] => IterateeT[IoResponse, IO, A]) =>
-  //             Input[IoRequested] =>
-  //             IterateeT[IoRequested, IO, StepT[IoResponse, IO, A]]) = {
-  //         k => in => {
-  //           in(
-  //             el = _ => drainNextResponseOrContOrDone(rsps, reqs, ex, k, in),
-  //             empty = drainNextResponseOrContOrDone(rsps, reqs, ex, k, in),
-  //             eof = drainNextResponseOrContOrDone(rsps, reqs, ex, k, in))
-  //         }
-  //       }
-
-  //       doneOr(loop(Vector.empty, Vector.empty))
-  //     }
-  //   }
-
-  // type LatLonResponse = RecPlus[\/[NonEmptyList[Throwable],(Double, Double)]]
-  // type IoLatLonResponse = IoExceptionOr[LatLonResponse]
-
-  // def getLatLon: EnumerateeT[IoResponse, IoLatLonResponse, IO] =
-  //   Iteratee.map { iollrsp =>
-  //     iollrsp map {
-  //       case (recNum, sep, rec, vel) =>
-  //         (recNum,
-  //           sep,
-  //           rec,
-  //           vel flatMap (el =>
-  //             TownshipGeocoder.getLatLon(el).leftMap(NonEmptyList(_))))
-  //     }
-  //   }
-
 }
