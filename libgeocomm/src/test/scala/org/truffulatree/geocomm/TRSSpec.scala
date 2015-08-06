7ee2efbabc3e22d4a7207f388da4c647d6ba95df
Martin Pokorny <martin@truffulatree.org>
2015-08-06 15:38:48 -0600

    Test code cleanup

--- libgeocomm/src/test/scala/org/truffulatree/geocomm/TRSSpec.scala
+++ libgeocomm/src/test/scala/org/truffulatree/geocomm/TRSSpec.scala
@@ -13,24 +13,28 @@ import org.scalatest.exceptions.TestFailedException
 class TRSSpec extends UnitSpec with PartialFunctionValues {
 
   "TRS Fraction type" should {
+    val fractionMin = 0
+    val fractionMax = 3
+
     "be represented by integers in {0,1,2,3}" in {
-      Tag.unwrap(TRS.Fraction.valueAt(0)) should equal (0)
-      Tag.unwrap(TRS.Fraction.valueAt(1)) should equal (1)
-      Tag.unwrap(TRS.Fraction.valueAt(2)) should equal (2)
-      Tag.unwrap(TRS.Fraction.valueAt(3)) should equal (3)
+      val range = fractionMin to fractionMax
+      (range map (i => Tag.unwrap(TRS.Fraction.valueAt(i))) should contain
+        theSameElementsInOrderAs range)
     }
 
     "have a minimum value of 0" in {
-      TRS.FractionEnum.min should equal (Some(0))
+      TRS.FractionEnum.min should equal (Some(fractionMin))
     }
 
     "have a maximum value of 3" in {
-      TRS.FractionEnum.max should equal (Some(3))
+      TRS.FractionEnum.max should equal (Some(fractionMax))
     }
 
     "not allow integers outside of set {0,1,2,3}" in {
-      an[TestFailedException] should be thrownBy TRS.Fraction.valueAt(-1)
-      an[TestFailedException] should be thrownBy TRS.Fraction.valueAt(4)
+      (an[TestFailedException] should be thrownBy 
+        TRS.Fraction.valueAt(fractionMin - 1))
+      (an[TestFailedException] should be thrownBy
+        TRS.Fraction.valueAt(fractionMax + 1))
     }
   }
 
@@ -40,7 +44,7 @@ class TRSSpec extends UnitSpec with PartialFunctionValues {
 
     "be represented by integers in range [1,2,...,36]" in {
       val range = sectionMin to sectionMax
-      (range map (i => Tag.unwrap(TRS.Section.valueAt(i))) should contain 
+      (range map (i => Tag.unwrap(TRS.Section.valueAt(i))) should contain
         theSameElementsInOrderAs range)
     }
 
