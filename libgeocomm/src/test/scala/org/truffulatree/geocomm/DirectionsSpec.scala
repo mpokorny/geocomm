628ccc9b068e81bfd8e1dccbf3083df007796d1c
Martin Pokorny <martin@truffulatree.org>
2015-08-05 21:25:40 -0600

    Remove some string duplication

--- libgeocomm/src/test/scala/org/truffulatree/geocomm/DirectionsSpec.scala
+++ libgeocomm/src/test/scala/org/truffulatree/geocomm/DirectionsSpec.scala
@@ -1,35 +1,44 @@
 package org.truffulatree.geocomm
 
 class DirectionsSpec extends UnitSpec {
+  val n = "N"
+  val e = "E"
+  val s = "S"
+  val w = "W"
+  val nw = "NW"
+  val ne = "NE"
+  val se = "SE"
+  val sw = "SW"
+
   "Directions" should {
     "have conventional string abbreviation representations" in {
-      Directions.North.toString should equal ("N")
-      Directions.East.toString should equal ("E")
-      Directions.South.toString should equal ("S")
-      Directions.West.toString should equal ("W")
-      Directions.NorthWest.toString should equal ("NW")
-      Directions.NorthEast.toString should equal ("NE")
-      Directions.SouthEast.toString should equal ("SE")
-      Directions.SouthWest.toString should equal ("SW")
+      Directions.North.toString should equal (n)
+      Directions.East.toString should equal (e)
+      Directions.South.toString should equal (s)
+      Directions.West.toString should equal (w)
+      Directions.NorthWest.toString should equal (nw)
+      Directions.NorthEast.toString should equal (ne)
+      Directions.SouthEast.toString should equal (se)
+      Directions.SouthWest.toString should equal (sw)
     }
 
     "should convert from string abbreviations to Enumeration value" in {
-      Directions("N") should equal (Directions.North)
-      Directions("n") should equal (Directions.North)
-      Directions("E") should equal (Directions.East)
-      Directions("e") should equal (Directions.East)
-      Directions("S") should equal (Directions.South)
-      Directions("s") should equal (Directions.South)
-      Directions("W") should equal (Directions.West)
-      Directions("w") should equal (Directions.West)
-      Directions("NW") should equal (Directions.NorthWest)
-      Directions("nw") should equal (Directions.NorthWest)
-      Directions("SW") should equal (Directions.SouthWest)
-      Directions("sw") should equal (Directions.SouthWest)
-      Directions("NE") should equal (Directions.NorthEast)
-      Directions("ne") should equal (Directions.NorthEast)
-      Directions("SE") should equal (Directions.SouthEast)
-      Directions("se") should equal (Directions.SouthEast)
+      Directions(n) should equal (Directions.North)
+      Directions(n.toLowerCase) should equal (Directions.North)
+      Directions(e) should equal (Directions.East)
+      Directions(e.toLowerCase) should equal (Directions.East)
+      Directions(s) should equal (Directions.South)
+      Directions(s.toLowerCase) should equal (Directions.South)
+      Directions(w) should equal (Directions.West)
+      Directions(w.toLowerCase) should equal (Directions.West)
+      Directions(nw) should equal (Directions.NorthWest)
+      Directions(nw.toLowerCase) should equal (Directions.NorthWest)
+      Directions(sw) should equal (Directions.SouthWest)
+      Directions(sw.toLowerCase) should equal (Directions.SouthWest)
+      Directions(ne) should equal (Directions.NorthEast)
+      Directions(ne.toLowerCase) should equal (Directions.NorthEast)
+      Directions(se) should equal (Directions.SouthEast)
+      Directions(se.toLowerCase) should equal (Directions.SouthEast)
     }
   }
 }
