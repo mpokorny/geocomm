21accb3f8eccf1798c7f26238938b566713bebd8
Martin Pokorny <martin@truffulatree.org>
2015-08-07 20:52:03 -0600

    Adapt unit tests for CSV API changes

--- csv2LatLon/src/test/scala/org/truffulatree/geocomm/CSVSpec.scala
+++ csv2LatLon/src/test/scala/org/truffulatree/geocomm/CSVSpec.scala
@@ -33,33 +33,33 @@ class CSVSpec extends UnitSpec {
 
   "CSV parsing" should {
     "convert 'NM' as the 'State' to New Mexico" in {
-      CSV.getState(basicRecord) should equal (Success(States.NewMexico))
+      CSV.convertState(basicRecord) should equal (Success(States.NewMexico))
     }
 
     "default to NM for the 'State' field value" in {
-      (CSV.getState(basicRecord - Columns.State.toString) should equal
+      (CSV.convertState(basicRecord - Columns.State.toString) should equal
         (Success(States.NewMexico)))
     }
 
     "convert 'NewMexico' as the 'Principal Meridian' to New Mexico PM" in {
-      (CSV.getPrincipalMeridian(basicRecord) should
+      (CSV.convertPrincipalMeridian(basicRecord) should
         equal (Success(PrincipalMeridians.NewMexico)))
     }
 
     "convert '23' as the 'Principal Meridian' to NewMexico PM" in {
-      (CSV.getPrincipalMeridian(basicRecord + ("Principal Meridian" -> "23"))
+      (CSV.convertPrincipalMeridian(basicRecord + ("Principal Meridian" -> "23"))
         should equal (Success(PrincipalMeridians.NewMexico)))
     }
 
     "default to New Mexico PM for the 'Principal Meridian' field value" in {
-      (CSV.getState(basicRecord - Columns.PrincipalMeridian.toString)
+      (CSV.convertState(basicRecord - Columns.PrincipalMeridian.toString)
         should equal (Success(States.NewMexico)))
     }
 
     "accept values 0-3 for the 'Township Fraction' field" in {
       val range = 0 to 3
       ((range map (
-        i => CSV.getTRSFraction(
+        i => CSV.convertTRSFraction(
           basicRecord + (Columns.TownshipFraction.toString -> i.toString),
           CSV.Columns.TownshipFraction)))
         should contain theSameElementsInOrderAs
@@ -67,7 +67,7 @@ class CSVSpec extends UnitSpec {
     }
 
     "default to 0 for the 'Township Fraction' field" in {
-      CSV.getTRSFraction(
+      CSV.convertTRSFraction(
         basicRecord - Columns.TownshipFraction.toString,
         CSV.Columns.TownshipFraction) should equal (Success(TRS.Fraction(0)))
     }
