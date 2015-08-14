ad1a122c62fb7cd479fc1ac8f5e86c9f0d4421f6
Martin Pokorny <martin@truffulatree.org>
2015-08-14 14:39:30 -0600

    Put into nmbg package

--- csv2LatLon/src/test/scala/org/truffulatree/geocomm/CSVSpec.scala
+++ csv2LatLon/src/test/scala/org/truffulatree/geocomm/CSVSpec.scala
@@ -4,8 +4,9 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
 //
-package org.truffulatree.geocomm
+package org.truffulatree.geocomm.nmbg
 
+import org.truffulatree.geocomm._
 import scala.collection.immutable.SortedMap
 import scala.math.Ordering
 import scalaz._
